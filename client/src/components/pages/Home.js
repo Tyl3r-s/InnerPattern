604f1a4a5b821ae1d14e0f04e845b58692b4bec9
Tyler <Tyl3r.s@outlook.com>
2022-10-04 14:56:03 -0400

    add landing page/style elements

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LandingLogo from '../../assets/mainlogo.png'
 
 const Home = () => {
   return (
-      <li className="mx-1">
-        <Link to="/JoinUs">
-          Start!
-        </Link>
-      </li>
+    <div className="LandingContainer">
+      <div className="divContainer">
+        <img src={LandingLogo} className="LandingLogo"></img>
+        <p className="LandingText">Welcome to InnerPattern, where we strive to understand ourselves better, become more mindful, and in the process modulate the thought patterns that are generating our suffering. </p>
+        <div className="LandingBtn">
+          <Link to="/JoinUs" style={{ textDecoration: 'none', color: 'white' }}>
+            Start!
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
