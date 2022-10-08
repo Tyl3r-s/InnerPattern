eb25429b987c7fb1626e6f2bef7396c5ce2e4cdd
Omar Rodriguez <omar.rguez26@gmail.com>
2022-10-08 11:24:46 -0400

    fix Nav bug

--- InnerPattern-main/client/src/components/pages/Navigation.js
+++ InnerPattern-main/client/src/components/pages/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import navLogo from "../../assets/navlogo.png";
