afb78d9d0420f4fda679d26a43b5eced39915961
Tyler <Tyl3r.s@outlook.com>
2022-10-10 09:58:37 -0400

    update resolvers

--- InnerPattern-main/server/schemas/resolvers.js
+++ InnerPattern-main/server/schemas/resolvers.js
@@ -5,79 +5,61 @@ const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
-    Query: {
-      // this queries me - the logged in user
-      me: async (parent, args, context) => {
-        if (context.user) {
-          const userData = await User.findOne({ _id: context.user._id }).select(
-            "-__v -password"
-          );
-          return userData;
-        }
-        throw new AuthenticationError("Not logged in");
-      },
-      entries: async (parent, args) => {
-        const entries = await Entry.find();
-  
-        return entries;
+  Query: {
+    // this queries me - the logged in user
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
       }
+      throw new AuthenticationError("Not logged in");
     },
+    entries: async (parent, args) => {
+      const entries = await Entry.find();
+
+      return entries;
+    }
+  },
+  entry: async (parent, { _id }) => {
+    return Entry.findOne({ _id });
+  },
     // mutations start here
     Mutation: {
-      // This is the login user mutation with authentication and returns a token and User
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw new AuthenticationError("Invalid credentials");
-        }
-  
-        const correctPassword = await user.isCorrectPassword(password);
-        if (!correctPassword) {
-          throw new AuthenticationError("Invalid credentials");
-        }
-        const token = signToken(user);
-  
-        return { token, user };
-      },
-      // this is the addUser muatation that returns a token and a user - used in the signup form
-      addUser: async (parent, args) => {
-        const user = await User.create(args);
-        const token = signToken(user);
-  
-        return { token, user };
-      },
-    // updateUser: async (parent, args, context) => {
-    //   if (context.user) {
-    //     return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-    //   }
+    // This is the login user mutation with authentication and returns a token and User
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-    //   throw new AuthenticationError('Not logged in');
-    // },
-    // login: async (parent, { email, password }) => {
-    //   const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Invalid credentials");
+      }
 
-    //   if (!user) {
-    //     throw new AuthenticationError("Invalid credentials");
-    //   }
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw new AuthenticationError("Invalid credentials");
+      }
+      const token = signToken(user);
 
-    //   const correctPassword = await user.isCorrectPassword(password);
-    //   if (!correctPassword) {
-    //     throw new AuthenticationError("Invalid credentials");
-    //   }
-    //   const token = signToken(user);
+      return { token, user };
+    },
+    // this is the addUser muatation that returns a token and a user - used in the signup form
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-    //   return { token, user };
-    // },
+      return { token, user };
+    },
+
     addEntry: async (parent, args, context) => {
       if (context.user) {
-        const entry = await Entry.create({...args, email: context.user.email });
+        const entry = await Entry.create({ ...args, email: context.user.email });
         await User.findByIdAndUpdate(
           {
             _id: context.user._id
           },
           {
-            $push: { entries: entry._id}
+            $push: { entries: entry._id }
           },
           {
             new: true
@@ -86,46 +68,8 @@ const resolvers = {
 
         return entry;
       }
-      throw new AuthenticationError('You need to be looged in!');
+      throw new AuthenticationError('You need to be logged in!');
     },
-    // editEntry: async (parent, {id, title, entryText, moodRating, email }, context) => {
-    //   if (context.user) {
-    //     const entry = await Entry.findByIdAndUpdate( {_id: id},
-    //       {title, entryText, moodRating}, {new: true});
-    //     // const entry = await Entry.create({...args, email: context.user.email });
-    //     await User.findByIdAndUpdate(
-    //       {
-    //         _id: context.user._id
-    //       },
-    //       {
-    //         $push: { entries: entry._id}
-    //       },
-    //       {
-    //         new: true
-    //       }
-    //     );
-
-    //     return entry;
-    //     // ELSE STATEMENT FOR TESTING - TO BE REMOVED
-    //   } else {
-    //     const entry = await Entry.findByIdAndUpdate( {_id: id},
-    //       {title, entryText, moodRating, email}, {new: true});
-    //     // const entry = await Entry.create({...args, email: context.user.email });
-    //     await User.findByIdAndUpdate(
-    //       {
-    //         email: args.email
-    //       },
-    //       {
-    //         $push: { entries: entry._id}
-    //       },
-    //       {
-    //         new: true
-    //       }
-    //     );
-
-    //     return entry;
-    //   }
-    // }
   }
 };
 
