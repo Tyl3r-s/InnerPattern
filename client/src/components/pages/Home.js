ddb67d0f992647346770f6a3a1c41ad887532053
Tyler <Tyl3r.s@outlook.com>
2022-10-12 11:23:52 -0400

    add auth to homepage

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,35 @@
 import LandingLogo from "../../assets/mainlogo.png";
 import React from "react";
+import Auth from '../../utils/auth';
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  if (Auth.loggedIn()) {
   return (
     <main>
+      <div className="LandingContainer">
+        <div className="divContainer">
+          <img
+            src={LandingLogo}
+            className="LandingLogo"
+            alt="landing page logo"
+          ></img>
+          <p className="LandingText">
+            Welcome to InnerPattern, where we strive to understand ourselves
+            better, become more mindful, and in the process modulate the thought
+            patterns that are generating our suffering.{" "}
+          </p>
+          <div className="btnContainer1">
+            <a href="/Profile" className="homeBtn">
+              Account
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  )} else {
+    return (
+      <main>
       <div className="LandingContainer">
         <div className="divContainer">
           <img
@@ -28,7 +53,8 @@ const Home = () => {
         </div>
       </div>
     </main>
-  );
+    )
+  }
 };
 
 export default Home;
